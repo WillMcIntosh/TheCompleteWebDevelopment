efc11891509f0d3201b9ef2415a855e1c1f51586
william <mcintoshwilliamh@gmail.com>
2017-09-04 18:15:44 -0400

    added html styling and switched to using external js. the <script> tag needs to be at the end of the body element so that the page will load and I can use .onclick

---
+++ JavaScript/js/changehtmlstyling.js
@@ -0,0 +1,8 @@
+// use <C+w>10- to rezise this window 10 lines smaller
+window.alert("hello this works")
+// something isn't working with it this way. I'll study more after this lesson
+
+document.getElementById("button1").onclick =
+    function(){
+        document.getElementById("facebook").style.width = '20px';
+    }
