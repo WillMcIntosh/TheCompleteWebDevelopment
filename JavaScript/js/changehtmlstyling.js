0910a6ef9472d3e18c53ffc5c9e63a8e64a17b1e
william <mcintoshwilliamh@gmail.com>
2017-09-05 08:28:01 -0400

    finished html styling with JS using an external JS file.

--- JavaScript/js/changehtmlstyling.js
+++ JavaScript/js/changehtmlstyling.js
@@ -1,8 +1,18 @@
 // use <C+w>10- to rezise this window 10 lines smaller
-window.alert("hello this works")
+
+// window.alert("hello this works")
+
 // something isn't working with it this way. I'll study more after this lesson
+// EDIT: 9/6/17 onClick was the issue. JS is case sensitive. It should be
+// onclick
 
 document.getElementById("button1").onclick =
     function(){
-        document.getElementById("facebook").style.width = '20px';
+        document.getElementById("facebook").style.width = '200px';
+        document.getElementById("facebook").style.backgroundColor = 'rgba( 26,203,135,0.922)';
+    }
+document.getElementById("button2").ondblclick =
+    function(){
+         document.getElementById("apple").style.fontFamily = 'monospace';
+         document.getElementById("apple").style.fontSize = '3em';
     }
