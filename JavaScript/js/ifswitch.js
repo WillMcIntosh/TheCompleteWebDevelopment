4401e79a8f245e305466ebe4af691103778b44ff
william <mcintoshwilliamh@gmail.com>
2017-09-11 11:06:41 -0400

    if and switch statements added

---
+++ JavaScript/js/ifswitch.js
@@ -0,0 +1,8 @@
+var speed =50;
+if(speed > 70){
+    window.alert("You are going too fast!");
+}elseif(speed < 40){
+    window.alert("You are going too slow");
+}else{
+    window.alert("Your speed is fine.");
+}
