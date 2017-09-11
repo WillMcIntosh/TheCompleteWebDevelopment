fa7dd1fd4d2589265b6e5cbdb881884f23cc73c6
william <mcintoshwilliamh@gmail.com>
2017-09-11 11:59:18 -0400

    switches and if blocks including user inputs. For a text user input .value.toLowerCase ensures word recognition if the user capitalises random words.

--- JavaScript/js/ifswitch.js
+++ JavaScript/js/ifswitch.js
@@ -1,8 +1,31 @@
-var speed =50;
-if(speed > 70){
-    window.alert("You are going too fast!");
-}elseif(speed < 40){
-    window.alert("You are going too slow");
-}else{
-    window.alert("Your speed is fine.");
+function checkSpeed(){
+    var speed =document.getElementById("speed").value;
+    if(speed > 70){
+        window.alert("You are going too fast!");
+    }else if(speed < 40){
+        window.alert("You are going too slow");
+    }else{
+        window.alert("Your speed is fine.");
+    }
+}
+
+function checkSeason(){
+    // to recognise user input capitals, change value toLowerCase
+    var x = document.getElementById("myInput").value.toLowerCase();
+    switch(x){
+        case "summer":
+            window.alert("It is summer time");
+            break;
+        case "winter":
+            window.alert("It is winter time");
+            break;
+        case "autumn":
+            window.alert("It is autumn");
+            break;
+        case "spring":
+            window.alert("It is spring time");
+            break;
+        default:
+            window.alert("I do not recognise this");
+    }
 }
