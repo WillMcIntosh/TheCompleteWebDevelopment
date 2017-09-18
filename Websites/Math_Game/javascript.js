a52372f5b3c543b3289db1c59c85aab1e6dddbec
william <mcintoshwilliamh@gmail.com>
2017-09-18 14:24:02 -0400

    got correct answer and random other answers generated then shuffled into the answer boxes using the Fisher-Yates shuffle algorithm. All that is left is responses when buttons are clicked

--- Websites/Math_Game/javascript.js
+++ Websites/Math_Game/javascript.js
@@ -47,26 +47,70 @@ function gameOver(){
     document.getElementById("startReset").innerHTML = "Start Game";
     gameOn = false;
 }
-
+//globally declare correctAnswer so it can be used in multiple functions
+var correctAnswer;
 
 function generateQA(){
-    /*
-     * var answers = [];
-     * for (var i =0; i=144; i++){
-     *     answers.push(i);
-     * }
-     */
+/*
+ * generate the question, correct answer, and then 3 false answers.
+ * add the four box answers to the boxes in random order.
+ */
+    
+// create an empty array and then populate it with 0 through 144
+    var answers = [];
+    for (var i =0; i<145; i++){
+        answers.push(i);
+    }
+// create the random first and second number, and then the correctAnswer 
     var firstNumber = Math.floor(Math.random()*13);
     var secondNumber = Math.floor(Math.random()*13);
-    var correctAnswer = firstNumber * secondNumber;
+    // assign value to correctAnswer
+    correctAnswer = firstNumber * secondNumber; 
+// find the index of correctAnswer and remove it from the array of possible answers
     var correctAnswerIndex = answers.indexOf(correctAnswer);
-    window.alert(correctAnswer);
-    window.alert(answers);
     if (correctAnswerIndex > -1){
         answers.splice(correctAnswerIndex,1)
     }
-    window.alert(answers);
-    document.getElementById("box1").innerHTML = correctAnswer;
-    document.getElementById("question").innerHTML = firstNumber + 'x' + secondNumber;
+// create an array of answer choices, including the correctAnswer
+// first, use the Fisher-Yates Shuffle algorithm
+    function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
 
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
+// next, shuffle the answers array and take the first 3 elements and add to new array
+    shuffle(answers);
+    var boxAnswers =[correctAnswer];
+    for(var j =0; j<3;j++){
+        boxAnswers.push(answers[j]);
+    }
+    shuffle(boxAnswers);
+// shuffling the new boxAnswers array now allows the elements to be added to the boxes
+    for(var i =1; i<5;i++){
+        document.getElementById("box"+i).innerHTML = boxAnswers[i -1];
+    }
+    document.getElementById("question").innerHTML = firstNumber + 'x' + secondNumber;
 }
+
+/*
+ * function answerBoxClick(){
+ *     if (gameOn == true){
+ * 
+ * 
+ *     }
+ * }
+ */
+
