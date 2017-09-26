ac0c4ffa82c3d750566012f82ca795b3e8d5c29b
william <mcintoshwilliamh@gmail.com>
2017-09-26 18:37:28 -0400

    ajax info, but it could not run some things locally. Noted errors to work out later.

---
+++ jQuery/js/ajax.js
@@ -0,0 +1,30 @@
+// AJAX = Asynchronous JavaScript and XML
+// can be used to update wb page without reloading the entire page
+// can send/receive data from a server after the page has loaded
+// can send data in the background
+//
+
+$(function(){
+    // load data from external file
+    // Note: does not work in chromium
+    $("#facebook").load("/home/will/Classes/Udemy_webdev/jQuery/ajax/somedata.txt");
+    // load only the first paragraph
+    $("#apple").load("/home/will/Classes/Udemy_webdev/jQuery/ajax/somedata.txt #paragraph1");
+    // this didn't load in chromium and firefox 
+    /*
+     * $("#button1").click(function(){
+     *     $.ajax({
+     *         url : "/home/will/Classes/Udemy_webdevjQuery/ajax/somedata.txt", success: function(data){
+     *             $("#microsoft").html(data);
+     *         }
+     *     });
+     * });
+     */
+    // got xml parsing error: junk after document element
+    $("#button1").click(function(){
+        $.ajax({url : "ajax/somedata.txt"}).done(function(data){
+            $("#microsoft").html(data);
+        });
+            });
+
+});
