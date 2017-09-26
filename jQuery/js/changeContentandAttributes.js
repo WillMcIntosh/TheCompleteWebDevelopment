a0b7603acd20f5c0702bb7923c367ebed28d4a80
william <mcintoshwilliamh@gmail.com>
2017-09-26 12:57:35 -0400

    changing html content and attributes of links, inputs.

--- jQuery/js/changeContentandAttributes.js
+++ jQuery/js/changeContentandAttributes.js
@@ -7,5 +7,29 @@ $(function(){
     $("#changeApple").click(function(){
         $("#apple").html("<p>This is Apple</p><p>New iOS available</p>");
     });
+    //change attributes of inputs
+    $("#changeInput").click(function(){
+        $("input").val("Sam");
+    });
+    // change attributes of a link
+    $("#changeMicrosoft").click(function(){
+        $("#microsoftLink").attr({"href": "http://google.com","title":"Google"});
+    });
+    // add text to an element
+    /*
+     * append text
+     * $("#addText").click(function(){
+     *     $("#microsoft").append("<ul><li>BMW</li><li>Mercedes</li></ul>");
+     * });
+     * add at beginning
+     * $("#addText").click(function(){
+     *    $("#microsoft").append("<ul><li>BMW</li><li>Mercedes</li></ul>");
+     * });
+     */
+     // after a paragraph
+    $("#addText").click(function(){
+        $("#microsoftParagraph").after("<p>New paragraph</p>");
+    });
+    // can also use .before for the same purpose
 
 });
