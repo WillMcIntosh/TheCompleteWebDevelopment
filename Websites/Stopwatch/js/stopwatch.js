0f3c2afc29b892f6e6d23b56c4f90726c252cddd
WillMcIntosh <mcintoshwilliamh@gmail.com>
2017-11-09 17:00:21 -0500

    start of stopwatch app, html and css finished with js strategy in comments

---
+++ Websites/Stopwatch/js/stopwatch.js
@@ -0,0 +1,50 @@
+//stopwatch.js
+    //variables
+        //App mode
+        //time counter
+        //lap counter
+        //variable for setInterval
+        //Number of Laps
+        
+        //minutes,seconds,centiseconds for time and lap
+    
+    //On App load show start and lap buttons
+    //click on startButton
+        //mode on
+        //show stop and lap buttons
+        //start counter
+
+    
+    //click on stopButton
+        //show resume and reset buttons
+        //stop counter
+    
+    //click on resumeButton
+        //show stop and lap buttons
+        //start counter
+    
+    //click on resetButton
+        //reload the page
+    
+    //click on lapButton
+        //if mode is ON
+            //stop action
+            //resetLap and print lap details
+            //start action
+    
+    //functions
+    //hideshowButtons function shows two buttons
+    
+    //start the counter
+    
+    //updateTime: converts counters to min,sec,centisec
+        //1min=60*100centiseconds=6000centiseconds
+        //1sec=100centiseconds
+        
+        //1min=60*100centiseconds=6000centiseconds
+        //1sec=100centiseconds
+            
+    //format numbers
+    
+    //addLap function: print lap details inside the lap box
+
