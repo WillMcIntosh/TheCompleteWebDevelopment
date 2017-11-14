7a0d954a231baec1b21da7d18e726221f0cd3bb4
WillMcIntosh <mcintoshwilliamh@gmail.com>
2017-11-14 16:13:15 -0500

    went back and added slider Jquery code. Renamed Math-Game and Math-Tutorial.

---
+++ Websites/Drawing-App/javascript.js
@@ -0,0 +1,11 @@
+$(function(){
+    $("#slider").slider({
+        min: 3,
+        max: 30,
+        slide: function(event, ui){
+            $("#circle").height(ui.value);
+            $("#circle").width(ui.value);
+        }
+
+    });
+});
