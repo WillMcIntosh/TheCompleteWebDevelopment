cc5cfd77d71e474323465dd70e5bde886454055a
WillMcIntosh <mcintoshwilliamh@gmail.com>
2017-11-29 14:30:13 -0500

    added JS code logic

--- Websites/Drawing-App/javascript.js
+++ Websites/Drawing-App/javascript.js
@@ -8,34 +8,4 @@ $(function(){
         }
     });
 
-    // for html5 canvas
-    var canvas = document.getElementById("paint");
-    var context = canvas.getContext('2d');
-
-    //draw a line
-
-    //declare a new path
-    context.beginPath();
-
-    // define line width
-    context.lineWidth = 40;
-    // define line colour
-    context.strokeStyle = '#42e565';
-
-    // set cap to the line (round, butt, square)
-    context.lineCap = 'round';
-    
-    // set line join style like bevel, round, miter
-    context.lineJoin = 'round';
-
-    // how far we are from left border, top border
-    context.moveTo(50,50);
-    // draw a straight line from starting point to a new position
-    context.lineTo(200,200);
-
-    //draw another line
-    context.lineTo(400,100);
-
-    //make line visible
-    context.stroke();
 });
