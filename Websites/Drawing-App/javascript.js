215194ab3ef786480dcc16092d633a56fa188770
WillMcIntosh <mcintoshwilliamh@gmail.com>
2017-11-29 16:02:33 -0500

    defined variables and added mousedown function to start

--- Websites/Drawing-App/javascript.js
+++ Websites/Drawing-App/javascript.js
@@ -7,5 +7,51 @@ $(function(){
             $("#circle").width(ui.value);
         }
     });
-
+    
+    //declare variables
+    var paint       = false;        // paintingerasing or not    
+    var paint_erase = "paint";      // painting or erasing
+    
+    //get the canvas and context
+    var canvas = 
+        document.getElementById("paint");    
+    var ctx    = canvas.getContext("2d"); 
+    
+    //get the canvas container
+    var container = $("#container");   
+        
+    //mouse position is an object
+    var mouse = {x: 0, y: 0};
+    
+    //onload load saved work from localStorage
+    
+    //set drawing parameters (lineWidth, lineJoin, lineCap)
+    ctx.lineWidth = 3;
+    ctx.lineJoin  = "round";
+    ctx.lineCap   = "round";
+    
+    //click inside container
+    container.mousedown(function(){
+        paint = true;
+    });
+    
+    //move the mouse while holding mouse key
+    
+                //get color input   
+                
+                //white color 
+                
+    //mouse up->we are not paintingerasing anymore
+    
+    //if we leave the container we are not paintingerasing anymore
+    
+    //click on the reset button
+    
+    //click on save button
+    
+    //click on the erase button
+    
+    //change color input
+    
+    //change lineWidth using slider
 });
