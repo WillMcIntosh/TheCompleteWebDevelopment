9e1b1729a597ea1f71c8190e1e98ba394d9a842a
WillMcIntosh <mcintoshwilliamh@gmail.com>
2017-11-30 13:58:52 -0500

    drawing mode, erase mode, and reset button logic added.

--- Websites/Drawing-App/javascript.js
+++ Websites/Drawing-App/javascript.js
@@ -31,25 +31,59 @@ $(function(){
     ctx.lineCap   = "round";
     
     //click inside container
-    container.mousedown(function(){
+    container.mousedown(function(e){
         paint = true;
+        ctx.beginPath();
+        mouse.x = e.pageX - this.offsetLeft;
+        mouse.y = e.pageY - this.offsetTop;
+        ctx.moveTo(mouse.x,mouse.y);
     });
     
     //move the mouse while holding mouse key
-    
-                //get color input   
-                
-                //white color 
-                
+    container.mousemove(function(e){
+        mouse.x = e.pageX - this.offsetLeft;
+        mouse.y = e.pageY - this.offsetTop;
+        if(paint == true){
+            if(paint_erase == "paint"){
+                // get color input
+                ctx.strokeStyle = 'red';
+            }else{
+                // white color
+                ctx.strokeStyle = 'white';
+            }
+            ctx.lineTo(mouse.x, mouse.y);
+            ctx.stroke();
+        }
+    });
+   
     //mouse up->we are not paintingerasing anymore
+    container.mouseup(function(){
+        paint = false;
+    });
     
     //if we leave the container we are not paintingerasing anymore
+    container.mouseleave(function(){
+        paint = false;
+    });
     
     //click on the reset button
+    $("#reset").click(function(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        paint_erase = "paint";
+        $("#erase").removeClass("eraseMode");
+    });
     
     //click on save button
     
     //click on the erase button
+    $("#erase").click(function(){
+        if(paint_erase == "paint"){
+            paint_erase = "erase";
+        }else{
+            paint_erase = "paint";
+        }
+        $(this).toggleClass("eraseMode");
+    });
     
     //change color input
     
