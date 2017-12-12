1104fc29e6f8e9bab4dec427a27640156214c05f
WillMcIntosh <mcintoshwilliamh@gmail.com>
2017-12-12 11:49:58 -0500

    started PHP chapter and added variables lesson

---
+++ PHP/embedphpinhtml.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Embedded PHP Example</title>
+</head>
+<body>
+   <h1>Embed PHP in HTML</h1>
+    <p>
+<?php
+    echo "This is a paragraph";
+
+?>
+    </p>
+<?php
+    echo "<p>This is also a paragraph</p>";
+?>
+
+</body>
+</html>
